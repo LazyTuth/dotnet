7c41218d7f0021790f96741b9c6a5eecdc81a3f4
LazyTuth <lazytuth@gmail.com>
2022-05-27 23:07:09 +0700

    quick initial CRUD Catalog project

---
+++ src/services/catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,44 @@
+﻿using Catalog.Api.Entities;
+using Catalog.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Catalog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _repository.GetProduct(id);
+            if (product == null)
+            {
+                _logger.LogError($"Product with Id:'{id}' not found.");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
